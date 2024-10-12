425c087ec4df35cbc55ad8d989230b75caad0fae
Tomas Pereira <tomaspereira2k@gmail.com>
2024-10-12 04:12:56 -0300

    Sanity CMS conectado y funcionando

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,22 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(), 
+    react(), 
+    sanity({
+      projectId: '3k9ety47',
+      dataset: 'production',
+      useCdn: false,
+      apiVersion: "2024-10-11",
+      studioBasePath: "/studio",
+      stega: {
+        studioUrl: "/studio",
+      },
+    })
+  ]
 });
---
+++ src/sanity/schema.ts
@@ -0,0 +1,34 @@
+// schemas/schema.ts lo saque de chatgpt
+import { defineType, defineField } from 'sanity'
+
+export const schema = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+})
